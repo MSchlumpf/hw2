4d789beb18fa4ead829d13dfea388d005d5f7d02
Michael Stumpf <Michael.Stumpf@uni-duesseldorf.de>
2015-11-05 11:25:25 +0100

    hw2 done

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,10 @@
+/* Homework2 by Michael Stumpf
+ * Changed solution from Lab2 to directly save some data
+ * in an output-file.
+ */
+
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,17 +13,24 @@ int main(){
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
-
+	ofstream out("output.dat"); //name of data-output file
+	
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
+	   double y[Nend-Nskip]; //create array for data-storage
+	   
+	   for(int i=1; i <= Nskip; i++){
+		   x = r*x*(1.0-x);	//for-loop for skipped results
+	   }
 	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
+	   		   x = r*x*(1.0-x);	//calculate x for storage
+			   y[i-Nskip-1] = x;	//store x in array y
+	   }
+	   for(int i=0; i <=(Nend-Nskip-1); i++){	//for-loop for storage of y	  
+		  out << r << "\t" << y[i] << endl;
+	   }
 	}
-
+		  out.close();
 
 	return 0;
 }
